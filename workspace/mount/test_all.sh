448cd0d59aef4b283056232038577439b106b2bf
Clstilmldy <celestialmelodygo@gmail.com>
2024-06-06 17:48:27 +0800

    cv1811h-sd-driver

---
+++ workspace/mount/test_all.sh
@@ -0,0 +1,14 @@
+# busybox echo "run busybox_testcode.sh"
+# ./busybox_testcode.sh
+# busybox echo "run iozone_testcode.sh"
+# ./iozone_testcode.sh
+# busybox echo "run libctest_testcode.sh"
+# ./libctest_testcode.sh
+# busybox echo "run libc-bench"
+# ./libc-bench
+# busybox echo "run lmbench_testcode.sh"
+# ./lmbench_testcode.sh
+# busybox echo "run lua_testcode.sh"
+# ./lua_testcode.sh
+busybox echo "run unixbench_testcode.sh"
+./unixbench_testcode.sh
